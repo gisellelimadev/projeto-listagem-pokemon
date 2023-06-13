cbe5cfaf9be6499453aca20d7b7a4c8f9ff8e89c
Giselle Lima <124682971+gisellelimadev@users.noreply.github.com>
2023-06-12 21:21:25 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+alert("Seja Bem-vindo, Treinador Pokemon");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
